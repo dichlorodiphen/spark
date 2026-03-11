99c83a72e612e8e495d0760afb65b4f6bdd2c81d
David Young <dichlorodiphen@gmail.com>
2026-03-11 23:26:40 +0000

    Fix Python test

--- python/pyspark/sql/tests/streaming/test_streaming_listener.py
+++ python/pyspark/sql/tests/streaming/test_streaming_listener.py
@@ -102,6 +102,7 @@ class StreamingListenerTestsMixin:
                     "latestOffset",
                     "addBatch",
                     "walCommit",
+                    "interbatch"
                 }
             )
         )
